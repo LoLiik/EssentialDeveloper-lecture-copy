b62316e04b917336df04719a43ab1c6bd4af7407
Evgenii Kulinichev <swift.lolik@gmail.com>
2020-10-12 12:44:01 +0300

    Delivers invalidData error on non 200 HTTP responses

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -10,70 +10,92 @@ import EssentialFeed
 
 
 class RemoteFeedLoaderTests: XCTestCase {
-    
+
     func test_init_doesNotRequestDataFromURL() {
         let (_, client) = makeSUT()
-        
+
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
-     
+
     func test_load_requestDataFromURL() {
         let url = URL(string: "http://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
-        
+
         sut.load() { _ in }
-        
+
         XCTAssertEqual(client.requestedURLs, [url])
     }
-    
+
     func test_loadTwice_requestDataFromURLTwice() {
         let url = URL(string: "http://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
-        
+
         sut.load() { _ in }
         sut.load() { _ in }
-        
+
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
-    
+
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
-        
+
         var capturedError: [RemoteFeedLoader.Error] = []
         sut.load { capturedError.append($0) }
-        
+
         let clientError = NSError(domain: "Test", code: 0)
         client.complete(with: clientError)
-        
+
         XCTAssertEqual(capturedError, [.connectivity])
     }
-    
+
+    func test_load_deliversErrorOnNon200HTTPResponse() {
+        let (sut, client) = makeSUT()
+
+        var capturedError: [RemoteFeedLoader.Error] = []
+        sut.load { capturedError.append($0) }
+
+        client.complete(withStatusCode: 400)
+
+        XCTAssertEqual(capturedError, [.invalidData])
+    }
+
     // MARK: - Helpers
-    
+
     private func makeSUT(url: URL = URL(string: "http://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(url: url, client: client)
         return (sut, client)
     }
-    
+
     private class HTTPClientSpy: HTTPClient {
-        
-        var completions: [(Error) -> Void] = []
-        
+
+        var completions: [(Error?, HTTPURLResponse?) -> Void] = []
+
         var requestedURLs: [URL] {
-            messages.map{ $0.url }
+            messages.map { $0.url }
         }
-        
-        private var messages: [(url: URL, completion: (Error) -> Void)] = []
-        
-        func get(from url: URL, completion: @escaping (Error) -> Void) {
+
+        private var messages: [(url: URL, completion: (Error?, HTTPURLResponse?) -> Void)] = []
+
+        func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
             messages.append((url, completion))
         }
-        
+
         func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(error)
+            messages[index].completion(error, nil)
         }
-        
+
+        func complete(withStatusCode code: Int, at index: Int = 0) {
+            let response = HTTPURLResponse(
+                url: requestedURLs[0],
+                statusCode: code,
+                httpVersion: nil,
+                headerFields: nil
+            )
+            messages[0].completion(nil, response)
+
+        }
+
     }
-    
+
 }
