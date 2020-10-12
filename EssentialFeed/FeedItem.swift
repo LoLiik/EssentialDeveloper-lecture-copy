7adf4870e6468ad2877a6af48eb50a76cfb34c45
Evgenii Kulinichev <swift.lolik@gmail.com>
2020-10-12 10:55:58 +0300

    Create FeedItem

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,17 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Евгений Кулиничев on 12.10.2020.
+//
+
+import Foundation
+
+struct FeedItem {
+    
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+    
+}
